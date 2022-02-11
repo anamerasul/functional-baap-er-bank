2aa138d40df8119aab39ee5e0ecd246a05b12cf5
anamerasul <anamerasul@gmail.com>
2022-02-11 15:10:18 +0600

    Update deposit and balance and handle string add

--- assets/js/banking.js
+++ assets/js/banking.js
@@ -5,15 +5,29 @@ document.getElementById('deposite-btn').addEventListener('click', function(e){
 
         const depositeInput=document.getElementById('user-deposite');
 
-        const depositeAmmout=depositeInput.value;
+        const depositeAmmoutText=depositeInput.value;
+
+        const depositeAmmout=parseFloat(depositeAmmoutText);
         console.log(depositeAmmout);
         // get current deposite
 
         const depositeTotal=document.getElementById('deposite-total');
         const depositeTotalText=depositeTotal.innerText;
 
-        depositeTotal.innerText=depositeAmmout
-        console.log(depositeTotalText)
+        const previousDepositeTotal=parseFloat(depositeTotalText);
+
+        depositeTotal.innerText=+previousDepositeTotal+depositeAmmout;
+        console.log(depositeTotalText);
+
+        // update balance
+        const balanceTotal=document.getElementById('balance-total');
+        const balanceTotalText=balanceTotal.innerText;
+
+        const previousBalanceTotal=parseFloat(balanceTotalText);
+
+        balanceTotal.innerText=previousBalanceTotal+depositeAmmout;
+
+
 
 
         // reset input
